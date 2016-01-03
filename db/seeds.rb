eca41771b98bc30b993ac6d378759d2d83104b2e
jophinjohn <jophinjohn@hotmail.com>
2016-01-03 04:38:01 +0000

    Second commit with sign up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+ # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+ # Create test profile
+member = User.new(
+  name: "member2015",
+  email:    "member@example.com",
+  password: "12345678"
+)
+member.skip_confirmation!
+member.save!
