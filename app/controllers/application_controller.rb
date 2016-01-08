cc343af1b0bf75c4e22d361109b8da3826371edf
jophinjohn <jophinjohn@hotmail.com>
2016-01-08 05:55:22 +0000

    Blocitoff Checkoint4 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+  
   protected
 
   def configure_permitted_parameters
@@ -11,4 +15,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) <<  :name
 
   end
+  
 end
