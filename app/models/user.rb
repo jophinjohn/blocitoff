1efe4114228374c74ca852a854d116f9141105b0
jophinjohn <jophinjohn@hotmail.com>
2016-01-11 02:18:47 +0000

    required name for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         has_many :items
+         
+  validates :name, presence: true
+    
 end
