60627718ab0ec116fc007402f11c7db648e2010a
jophinjohn <jophinjohn@hotmail.com>
2016-01-12 06:40:27 +0000

    Checkpoint 5 done complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+  def create
+   @user = User.find(params[:user_id])
+   @item = @user.items.build(items_params)
+     
+   if @item.save
+    redirect_to @user, notice: "Yes! New todo saved!"
+   else
+    redirect_to @user, error: "There was an error creating todo, please try again."
+   end
+  end
+    
+  def items_params
+    params.require(:item).permit(:name)
+  end
+  
 end
