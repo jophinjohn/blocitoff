d35b643a198c193155ce5b57493b5cabf3aef30d
jophinjohn <jophinjohn@hotmail.com>
2016-01-14 15:49:21 +0000

    Last try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @item = @user.items
+    respond_to do |format|
+       format.html
+       format.js
+     end
   end
   
 end
