790b5f4d7c609d0ca4d73bb3c7d25dbc1828b22d
Jophin <jophinjohn@hotmail.com>
2017-02-06 07:31:14 -0500

    First addition to new blocitoff githubpage

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,33 @@
+class ItemsController < ApplicationController
+  def create
+   @user = User.find(params[:user_id])
+   @item = @user.items.build(items_params)
+     
+   if @item.save
+    redirect_to @user, notice: "Yes! New todo saved!"
+   else
+    redirect_to @user, error: "There was an error creating todo, please try again."
+   end
+  end
+  def destroy
+   @user = User.find(params[:user_id])
+   @item = @user.items
+   
+   if @item.destroy
+     flash[:notice] = "Yes! Todo finished and deleted."
+   else
+    flash[:error] = "Oops! There was an error deleting that todo, try again."
+   end
+     
+   respond_to do |format|
+    format.html
+    format.js
+   end
+  end
+   
+  protected   
+  def items_params
+    params.require(:item).permit(:name)
+  end
+  
+end
